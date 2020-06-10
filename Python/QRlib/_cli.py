33d0aadef85e06a050e3cf2490adb1f551889970
Himanshu-Cyber-Code <66552157+Himanshu-Cyber-Code@users.noreply.github.com>
2020-06-10 11:02:43 +0530

    Finalized

--- Python/QRlib/_cli.py
+++ Python/QRlib/_cli.py
@@ -18,4 +18,24 @@ def Cline():
         os.system("python -m pip install --upgrade QRlib")
     
     else:
+        print("Got An Unexpected Argument")import sys
+import os
+
+def Cline():
+    
+    if(len(sys.argv)<2):
+        print('an argumet is required ("-h" or "--help")')
+        sys.exit()
+        
+        
+    if('-h' in sys.argv or '--help' in sys.argv):
+        print(os.system("pip show QRlib"))
+        print("")
+        print("")
+        print(help("QRlib"))
+        
+    elif('-u' in sys.argv or '--update' in sys.argv):
+        os.system("python -m pip install --upgrade QRlib")
+    
+    else:
         print("Got An Unexpected Argument")
